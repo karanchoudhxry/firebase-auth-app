64d84f33a51de8ee1a92035e7fefbbd386d3bbf8
Karan Choudhary <karanchoudhary0826@gmail.com>
2021-06-07 12:53:50 +0530

    Update app.js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -15,18 +15,3 @@ const provider = new firebase.auth.GoogleAuthProvider();
 signInBtn.onclick = () => auth.signInWithPopup(provider);
 
 signOutBtn.onclick = () => auth.signOut();
-
-auth.onAuthStateChanged((user) => {
-  if (user) {
-    // signed in
-    whenSignedIn.hidden = false;
-    signup.hidden = true;
-    userDetails.innerHTML = `<img src="${user.photoURL}"><div class="userInfo"><h3>${user.displayName}</h3><p>${user.email}</p></div>`;
-    document.getElementById("signup").style.display = "none";
-  } else {
-    // not signed in
-    whenSignedIn.hidden = true;
-    signup.hidden = false;
-    userDetails.innerHTML = "";
-  }
-});
